4c7c16f49e6bee1a94accfa831a2a36723e2ca0a
Andrew Woo <andrew.woo@verizon.net>
2019-06-20 23:12:10 -0400

    working on algo

--- find-pair.js
+++ find-pair.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+/*const fs = require("fs");
 
 for (let i = 0; i < process.argv.length; i++) {
   console.log(process.argv[i]);
@@ -7,4 +7,23 @@ for (let i = 0; i < process.argv.length; i++) {
 fs.readFile(process.argv[0], function(err, data) {
   //returns a buffer... hmmm
   console.log(data);
-});
+});*/
+
+//work on algo. while learning node.js
+//when reading in text file convert array of array
+const pricesText = [
+  ["Candy Bar", 500],
+  ["Paperback Book", 700],
+  ["Detergent", 1000],
+  ["Headphones", 1400],
+  ["Earmuffs", 2000],
+  ["BluetoothStereo", 6000]
+];
+
+/*  
+    Brute Force - sort in descending order based on pricesText[i][1] - O(n log n)
+    Iterate through the array for two values adding up to gift card value or under. O(n)
+    Highest two values is the correct answer. If only one or no items, return Not Possible 
+    Runtime - O(n log n)
+    Spacetime - O(n)
+*/
